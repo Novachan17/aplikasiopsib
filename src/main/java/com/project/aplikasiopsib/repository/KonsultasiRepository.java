873bd373d4bdb89b33e32794fa2a9f6794d4bb8e
Nova Indriani <160774395+Novachan17@users.noreply.github.com>
2025-12-28 14:24:00 +0700

    Update KonsultasiRepository.java

--- src/main/java/com/project/aplikasiopsib/repository/KonsultasiRepository.java
+++ src/main/java/com/project/aplikasiopsib/repository/KonsultasiRepository.java
@@ -3,8 +3,14 @@ package com.project.aplikasiopsib.repository;
 import com.project.aplikasiopsib.model.Konsultasi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface KonsultasiRepository
         extends JpaRepository<Konsultasi, String> {
 
+    // 🔍 SEARCH BY ID KONSULTASI
+    List<Konsultasi> findByIdKonsultasiContainingIgnoreCase(String keyword);
+
+    // (sudah kamu pakai sebelumnya)
     Konsultasi findTopByOrderByIdKonsultasiDesc();
 }
